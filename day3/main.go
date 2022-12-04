447a87786cb5c75b9dd0e6d64a950d7dd4401afd
gart <gart.algar@gmail.com>
2022-12-04 23:23:57 +0100

    day3 part1

--- day3/main.go
+++ day3/main.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"log"
 	"os"
-//	"strings"
+	"strings"
 	"unicode/utf8"
+	"unicode"
 )
 
 var FILENAME = "input"
+var letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 func getFile() []string {
 	//open and read the file and return a slice with slices
@@ -29,9 +31,14 @@ func getFile() []string {
 func main() {
 	rucksacks := getFile()
 	fmt.Printf("\n : %v\n", rucksacks)
+	var total = 0
 	for _, line := range rucksacks {
-		splitLines(line)
+		var front, back = splitLines(line)
+		fmt.Printf("front is %s\n", front)
+		fmt.Printf("back is %s\n", back)
+		total += analyseItemPlacement(front, back)
 	}
+	fmt.Printf("total is %d\n", total)
 }
 
 func splitLines(line string) (string, string){
@@ -40,7 +47,23 @@ func splitLines(line string) (string, string){
 	fmt.Printf("line is %s\n", line)
 	var front = line[0:lineLength/2]
 	var back = line[lineLength/2:lineLength]
-	fmt.Printf("front is %s\n", front)
-	fmt.Printf("back is %s\n", back)
 	return front, back
+}
+
+func analyseItemPlacement(front string, back string) (int){
+	for _, rune := range front {
+		if(strings.ContainsRune(back, rune)) {
+			fmt.Printf("back contains this rune %q\n", rune)
+			return priority(rune)
+		}
+	}
+	return 0;
+}
+
+func priority(c rune) int {
+	if unicode.IsUpper(c) {
+		return int(c - 38)
+	} else {
+		return int(c - 96)
+	}
 }
