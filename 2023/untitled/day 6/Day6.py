252e5c2ac42a751739da97a237e63ddfe2baffa6
gartcimore <gart.algar@gmail.com>
2023-12-06 16:42:49 +0100

    day6 step 2

--- 2023/untitled/day 6/Day6.py
+++ 2023/untitled/day 6/Day6.py
@@ -26,6 +26,38 @@ class Day6(Day):
         # print(f"possible winnings: {possible_winnings.values()}")
         return prod(possible_winnings.values())
 
+    def solveStep2(self):
+        time = int(self.raw_items[0].split(":")[1].strip().replace(" ",""))
+        best_distance = int(self.raw_items[1].split(":")[1].strip().replace(" ",""))
+
+        possible_winnings = defaultdict(int)
+        start = 0
+        if (time % 2) == 0:
+            start = time / 2
+            print(f"start is {start} because even")
+        else:
+            start = (time -1) / 2
+            print(f"start is {start} because odd")
+
+        winning = True
+        win_count = 0
+        hold_time = start -1
+        while winning:
+            remaining_time = time - hold_time
+            distance = remaining_time * hold_time
+            # print(f"hold: {hold_time}, remaining: {remaining_time}, distance: {distance}")
+            if distance > best_distance:
+                win_count += 1
+                hold_time -= 1
+            else:
+                winning = False
+                print(f"hold time {hold_time}")
+        print(f"{start - 1 } => {hold_time + 1} == {start - hold_time }")
+
+        return win_count*2 + 1
+        # # print(f"possible winnings: {possible_winnings.values()}")
+        # return prod(possible_winnings.values())
+
 # Part 1
 day: Day6 = Day6(step=Step.STEP_1)
 print(f"Part#1 Test: {day.solveStep1()}")
@@ -35,6 +67,6 @@ print(f"Part#1 Prod: {day.solveStep1()}")
 
 # Part 2
 day: Day6= Day6(step=Step.STEP_2)
-print(f"Part#2 Test: {day.dummy()}")
+print(f"Part#2 Test: {day.solveStep2()}")
 day.prod_mode()
-print(f"Part#2 Prod: {day.dummy()}")
+print(f"Part#2 Prod: {day.solveStep2()}")
